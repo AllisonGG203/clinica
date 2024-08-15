f25e1b54e41ec25bca3d3424e6c3e5236b6f814f
AllisonGG203 <allisongg203@gmail.com>
2024-08-15 06:53:08 -0600

    actu

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,5 +1,9 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+   .vue() // Solo si usas Vue.js
+   .sass('resources/sass/app.scss', 'public/css')
+   .babelConfig({
+       presets: ['@babel/preset-env'],
+       sourceType: 'unambiguous' // Le dice a Babel cómo tratar los módulos
+   });
